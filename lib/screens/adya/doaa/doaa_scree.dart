2c8f6f51487901f32db32d98f332a8b8b193b3a7
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2023-09-27 06:22:33 +0300

    doaa screen & edits in adya files

--- lib/screens/adya/doaa/doaa_scree.dart
+++ lib/screens/adya/doaa/doaa_scree.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: no_logic_in_create_state
 
 import 'package:ana_muslim/models/doaa_model.dart';
+import 'package:ana_muslim/widgets/custom_inner_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class DoaaScreen extends StatefulWidget {
   final Object? doaaModel;
@@ -16,8 +18,52 @@ class DoaaScreenState extends State<DoaaScreen> {
   DoaaModel doaaModel;
   DoaaScreenState(this.doaaModel);
 
+  List<String> doaaString = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (doaaString.isEmpty) {
+      loadFile(doaaModel.index);
+    }
+
+    return CustomInnerScreen(
+      screenTitle: doaaModel.name,
+      juzNumber: "",
+      isSuraScreen: false,
+      isMakeah: false,
+      screenBody: screenBody(),
+    );
+  }
+
+  Widget screenBody() {
+    return Expanded(
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Center(
+            child: Text(
+              doaaString[index],
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          );
+        },
+        itemCount: doaaString.length,
+      ),
+    );
+  }
+
+  void loadFile(int index) async {
+    String file =
+        await rootBundle.loadString("assets/files/doaa/${index + 1}.txt");
+    List<String> suraLines = file.split("\n");
+
+    doaaString = [];
+    for (int i = 0; i < suraLines.length; i++) {
+      String line = suraLines[i];
+      String modifiedLine = line.trimRight();
+      doaaString.add(modifiedLine);
+    }
+
+    setState(() {});
   }
 }

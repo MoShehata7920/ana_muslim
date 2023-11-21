40c5b8feffbbe53132bc6d388566195ac1d7cbfc
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2023-11-21 02:13:44 +0200

    swipe sura from left to right

--- lib/screens/quran/sura/sura.dart
+++ lib/screens/quran/sura/sura.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: no_logic_in_create_state
-
 import 'package:flutter/material.dart';
-import 'package:card_swiper/card_swiper.dart';
 
 class SuraScreen extends StatelessWidget {
   final String startImageIndex;
@@ -28,21 +25,24 @@ class SuraScreen extends StatelessWidget {
       appBar: AppBar(
         toolbarHeight: 1,
       ),
-      body: Swiper(
-        loop: false,
+      body: PageView.builder(
         itemCount: imageFilenames.length,
+        reverse: true, 
+        scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
           return Center(
             child: Padding(
               padding: const EdgeInsets.only(bottom: 8),
-              child: Container(
-                height: MediaQuery.of(context).size.height - 30,
-                width: MediaQuery.of(context).size.width - 30,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(
-                      'assets/images/quran/${imageFilenames[index]}.png',
+              child: InteractiveViewer(
+                child: Container(
+                  height: MediaQuery.of(context).size.height - 30,
+                  width: MediaQuery.of(context).size.width - 30,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        'assets/images/quran/${imageFilenames[index]}.png',
+                      ),
                     ),
                   ),
                 ),
@@ -50,7 +50,6 @@ class SuraScreen extends StatelessWidget {
             ),
           );
         },
-        autoplay: false,
       ),
     );
   }
